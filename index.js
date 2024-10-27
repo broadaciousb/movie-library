0175a6e167d8a254442d583e9092c01c7a2c946f
Broady <broadybmx@gmail.com>
2024-10-27 11:57:11 -0400

    feat: start async function for calling movie api and mapping to html - not finished

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // DATA API: https://www.omdbapi.com/?i=tt3896198&apikey=f54b9d83
-// POSTER API: http://img.omdbapi.com/?apikey=[f54b9d83]&
+// POSTER API: http://img.omdbapi.com/?i=tt3896198&apikey=f54b9d83
 
+const movieListEl = document.querySelector(".movie__list");
 
 async function main() {
   const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=f54b9d83");
-  moviesData = await movies.json();
+  const moviesData = await movies.json();
   console.log(moviesData);
-  
+  movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
 }
 
 main();
+
+function movieHTML(movie) {
+  return `<div class="movie__wrapper">
+            <div class="movie">
+              <div class="movie__img--wrapper">
+                <img src="${movie.Poster}" alt="" class="movie__img">
+              </div>
+              <h3 class="movie__title">${movie.Title}</h3>
+              <div class="movie__release-date">${movie.Year}</div>
+            </div>
+          </div>`;
+}
