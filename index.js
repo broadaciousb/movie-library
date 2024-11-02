f5662fafaab66e213a54a723b65926a569af1832
Broady <broadybmx@gmail.com>
2024-11-02 12:40:48 -0400

    feat: Add javascript function for filtering results and searching for titles, search bar input is not integrated yet

--- index.js
+++ index.js
@@ -2,15 +2,56 @@
 // POSTER API: http://img.omdbapi.com/?i=tt3896198&apikey=f54b9d83
 
 const movieListEl = document.querySelector(".movie__list");
+const movieAPI = `https://www.omdbapi.com/?i=tt3896198&apikey=f54b9d83`;
 
-async function main() {
-  const movies = await fetch("https://www.omdbapi.com/?&i=tt3896198&apikey=f54b9d83&s='star'");
-  const moviesData = await movies.json();
-  console.log(moviesData);
-  movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+var filter = ``;
+
+const tooManyResults = '<h3 class="too-many-results">Too many results, narrow your search please.</h3>';
+
+const movieNotFound = '<h3 class="movie-not-found">Movie not found, try again.</h3>';
+
+function search() {
+  var newSearch = 'game'
+  var newAPI = movieAPI + `&s=${newSearch}` + filter;
+  main(newAPI);
 }
 
-main();
+function filterMovies(event) {
+  if (event.target.value === 'movie') {
+    filter = '&type=movie';
+  }
+  else if (event.target.value === 'series') {
+    filter = '&type=series';
+  }
+  else if (event.target.value === 'episode') {
+    filter = '&type=episode';
+  }
+  else {
+    filter = ''
+  }
+  console.log(event.target.value);
+  console.log(filter);
+  search();
+}
+
+async function main(API) {
+  const movies = await fetch(API);
+  const moviesData = await movies.json();
+  console.log(moviesData);
+  if (moviesData.Error === 'Too many results.') {
+    movieListEl.innerHTML = tooManyResults;
+  }
+  else if (moviesData.Error === 'Movie not found!') {
+    movieListEl.innerHTML = movieNotFound;
+  }
+  else {
+    movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+  }
+}
+
+
+search();
+
 
 function movieHTML(movie) {
   return `<div class="movie__wrapper">
