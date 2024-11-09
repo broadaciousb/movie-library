a076f2529a6be62393ef9257b6964122001988a4
Broady <broadybmx@gmail.com>
2024-11-09 13:07:12 -0500

    feat: Finish modal for index.html, start toggleModal function, function not completed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const contactButton = document.querySelector(".nav__contact");
+let isModalOpen = false;
 const homeSearchInput = document.querySelector("#home-search__input");
 const homeSearchButton = document.querySelector("#home-search__button");
-
+const modal = document.querySelector(".modal");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
 
 function homeSearch(search) {
-  localStorage.setItem("search", search)
+  localStorage.setItem("search", search);
   window.location.href = `${window.location.origin}/find_movies.html`;
-};
+}
 
 homeSearchButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -17,3 +21,17 @@ homeSearchInput.addEventListener("keypress", (e) => {
     homeSearch(homeSearchInput.value);
   }
 });
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    document.modal.classList += " display-none";
+    document.nav.classList.remove("display-none");
+    document.header.classList.remove("display-none");
+  } else {
+    isModalOpen = true;
+    document.modal.classList.remove("display-none");
+    document.nav.classList += " display-none";
+    document.header.classList += " display-none";
+  }
+}
